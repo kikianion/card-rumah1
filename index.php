ab423a37791d2db39094313527175db4b24c8ab7
nuha <nuha@harap.id>
2021-08-07 14:30:52 +0700

    change link demo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="UTF-8" />
+        <title>Homer Simpson</title>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" href="font-awesome.css" />
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+        <link rel="icon" href="" type="image/x-icon" />
+    </head>
+
+    <body>
+        <img id="userPhoto" src="https://i.imgur.com/t8ZX9um.jpg" alt="User Photo"/>
+        <h1>Homer Simpson</h1>
+        <div id="description">
+            Homer Jay Simpson is a fictional character and the protagonist of the
+            American animated sitcom The Simpsons. LinkFree template created by
+            <a href="https://ryanloi.me/" target="_blank">Ryan Loi</a>.
+        </div>
+        <div id="links">
+            <a class="link"
+                href="geo:37.786971,-122.399677;u=35"
+                target="_blank">open map</a>
+        </div>
+
+        <!-- Javascript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </body>
+</html>
