8a4a871e1028b03116423fcddb1f5d9933dec0d2
nuha <nuha@harap.id>
2021-08-07 19:34:46 +0700

    fontawesom and new list

--- index.php
+++ index.php
@@ -5,26 +5,73 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="UTF-8" />
         <title>Homer Simpson</title>
-        <link rel="stylesheet" href="style.css" />
-        <link rel="stylesheet" href="font-awesome.css" />
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
+        <!--<link rel="stylesheet" href="style.css" />-->
+        <!--<link rel="stylesheet" href="font-awesome.css" />-->
+        <!--<link rel="stylesheet" href="assets/css/font-awesome.min.css" />-->
         <link rel="icon" href="" type="image/x-icon" />
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="lib/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">-->
+
+        <!-- Latest compiled and minified JavaScript -->
+        <link rel="stylesheet" href="lib/fontawesome-free-5.15.4-web/css/all.min.css" >
+        <style>
+            .btn1{
+                border-radius: 20px; background-color: #3b6978; color: white;
+                margin-bottom: 15px ;
+
+            }
+        </style>
     </head>
 
-    <body>
-        <img id="userPhoto" src="https://i.imgur.com/t8ZX9um.jpg" alt="User Photo"/>
-        <h1>Homer Simpson</h1>
-        <div id="description">
-            Homer Jay Simpson is a fictional character and the protagonist of the
-            American animated sitcom The Simpsons. LinkFree template created by
-            <a href="https://ryanloi.me/" target="_blank">Ryan Loi</a>.
+    <body style="background-color:#eee">
+        <div class="container">
+            <div class="row" style="margin:40px">
+                <div class="col-md-2 col-md-offset-5 text-center" style="xborder: 1px solid black; ">
+                    <img id="userPhoto" src="https://i.imgur.com/t8ZX9um.jpg" alt="User Photo" style="width: 100px; border:4px solid black; border-radius: 25px 25px 25px 25px"/>
+                </div>
+
+            </div>
+            <div class="row text-center" style="margin-bottom: 20px">
+                <div class="col-md-12">
+                    <h1 style="font-weight: bolder">Nuha / Dzikri Ulinnuha AZ</h1>
+                </div>
+                <div id="description" style="font-size: 1.3em;">
+                    Programmer, coder,computer system builder, tinggal di desa Sukolilo
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="btn-group btn-group-justified" role="group" aria-label="...">
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-default btn-block btn-lg btn1" href="geo:-7.083852, 112.328941;u=35" target="_blank" style="">Google Map</a>
+                        </div>
+                        <div class="btn-group" role="group">
+                            <a class="btn btn-default btn-block btn-lg btn1" href="google.navigation:q=-7.083852, 112.328941" target="_blank" style="">Arah</a>
+                        </div>
+                    </div>
+                    <a class="btn btn-default btn-block btn-lg btn1" href="https://www.instagram.com/p/CSQ6qgmBVfq/?utm_source=ig_web_copy_link" target="_blank" style="">Foto Rumah</a>
+
+                </div>
+
+            </div>
         </div>
-        <div id="links">
-            <a class="link" href="geo:37.786971,-122.399677;u=35" target="_blank">open map</a>
-            <a class="link" href="google.navigation:q=San+Francisco" target="_blank">Nav</a>
+        <div style="position: fixed; bottom: 0; text-align: center; width: 100%">
+            <i class="fa fa-copyright"></i> Harapid System | Sukolilo Sukodadi Lamongan | 081336491544 | kikianion@gmail.com
         </div>
+        <!--        <h1>Homer Simpson</h1>
+                <div id="description">
+                    Homer Jay Simpson is a fictional character and the protagonist of the
+                    American animated sitcom The Simpsons. LinkFree template created by
+                    <a href="https://ryanloi.me/" target="_blank">Ryan Loi</a>.
+                </div>-->
+        <!--        <div id="links">
+                </div>-->
 
         <!-- Javascript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="lib/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     </body>
 </html>
